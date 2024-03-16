7f8aa1e87f48afd0b695a0c50e82c65ec946a136
Brian Garcia <brian.garcia@encoura.org>
2024-03-15 21:53:34 -0400

    Removing lightbox

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { getPosts } from '../utils/mdx-utils';
 
 import Footer from '../components/Footer';
 import Header from '../components/Header';
-import Layout, { GradientBackground } from '../components/Layout';
 import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
 import SEO from '../components/SEO';
